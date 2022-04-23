88206ce0aace30fa26feff004514a58209c3087b
Marcin-Migdal <mortimor132@gmail.com>
2022-04-23 17:35:26 +0200

    updated useGetData custom hook

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,5 +1,13 @@
 import "./Dropdown.css";
 
-export const Dropdown = () => {
-    return <div className="dropdown-container">Dropdown</div>;
+export const Dropdown = ({ options }) => {
+    return (
+        <div className="dropdown-container">
+            <ul>
+                {options.map((o) => (
+                    <li key={o.url}>{o.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 };
--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,21 +1,37 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export const useGetData = ({ url }) => {
+export const useGetData = ({url, id, infiniteScroll = false}) => {
     const [data, setData] = useState(undefined);
 
     useEffect(() => {
-        getData();
-    }, []);
+        !!url ? getData() : setData(null);
+    }, [url, id]);
 
     const getData = async () => {
         try {
-            const response = await axios.get(url); 
-            setData(response.data.results);
+            let responseData;
+
+            if (!!id) {
+                const response = await axios.get(`${url}/${id}`);
+                responseData = response.data;
+            } else {
+                const response = await axios.get(url);
+                const resData = response.data;
+
+                if (!infiniteScroll) responseData = resData;
+                else {
+                    if (!!data) responseData = { ...resData, results: [...data.results, resData.results] };
+                    else responseData = resData;
+                }
+            } 
+
+            setData(responseData);
         } catch (e) {
+            setData(null);
             console.log(e);
         }
     };
 
-    return { data, setData };
+    return { data, setData, getData };
 };
